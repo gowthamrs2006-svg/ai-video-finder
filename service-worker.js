90349bf075db5cb9bb1673fb6d594d05632bf8ff
gowthamrs2006-svg <gowthamrs2006@gmail.com>
2026-01-22 10:00:40 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,22 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
+const CACHE_NAME = "gm-video-finder-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
