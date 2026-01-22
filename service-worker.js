7ee29910d73c3fbd60b56e0388e745e4231c20bc
gowthamrs2006-svg <gowthamrs2006@gmail.com>
2026-01-22 10:18:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,40 @@
-const CACHE_NAME = "gm-video-finder-cache-v1";
-const urlsToCache = [
+const CACHE_NAME = "gm-video-finder-v1";
+
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
